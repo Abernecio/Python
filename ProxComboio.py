645a7227c2c188fe43c9528bdfb404eeff03d0ca
Abernecio <Abernecio@github.com>
2018-10-24 22:58:40 +0100

    Minor edits

--- ProxComboio.py
+++ ProxComboio.py
@@ -1,30 +1,44 @@
 import requests
-import os
-import string
 from bs4 import BeautifulSoup
 import datetime
-import time
-import lxml
+import calendar
+
+url_stations = 'https://www.cp.pt/sites/spring/stations'
+
+r_stations = requests.get(url_stations)
+
+stations = r_stations.json()
 
 
-def get_todays_trains():
+def get_todays_trains(depart, arrival):
+
+    get_todays_trains.depart = stations
+    get_todays_trains.arrival = stations
     now = datetime.datetime.now()
+    dayn = now.day
+    monthno = now.month
+    yearno = now.year
 
     url = 'https://www.cp.pt/sites/passageiros/en/train-times/Train-time-results'
 
     r = requests.post(url, allow_redirects=False, data={
-        'arrival': 'Porto - Campanha',
-        'depart': 'Aguas Santas - Palmilheira',
+        'arrival': arrival,
+        'depart': depart,
+        'date': str(dayn) + ' ' + calendar.month_name[monthno] + ', ' + str(yearno),
         'departDate': str(now.year) + '-' + str(now.month) + '-' + str(now.day)
     })
 
     html = r.text
     soup = BeautifulSoup(html, features='lxml')
 
+    train_data = dict()
+    train_data['departing_date'] = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
+    train_data['departing_trains'] = []
     for row in soup.findAll('table')[1].tbody.findAll('tr'):
         depart = row.findAll('td')[2].text.split()
-    print(depart)
-    return depart
+        train_data['departing_trains'].append(depart)
+    print(train_data)
+    return train_data
 
 
-get_todays_trains()
+get_todays_trains('Porto - Campanha', 'Porto - Sao Bento')
